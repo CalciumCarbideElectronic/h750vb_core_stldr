c9a80e83044b73302f58f021676d6e2660d76ba8
Liuhancheng <cn_lhc@qq.com>
2020-03-08 18:30:51 +0800

    existing code base

---
+++ Core/Inc/spidriver.h
@@ -0,0 +1,147 @@
+#ifndef __STM32_EVAL_SPI_FLASH_H
+#define __STM32_EVAL_SPI_FLASH_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+
+#include "stm32h750xx.h"
+#include "main.h"
+
+#define sFLASH_CMD_RDJDID 0x9F
+#define sFLASH_CMD_RDMDID 0x9F
+#define sFLASH_CMD_RDUID 0x4B
+
+
+
+#define sFLASH_CMD_WRITE          0x02
+#define sFLASH_CMD_WRSR           0x01
+#define sFLASH_CMD_WREN           0x06
+#define sFLASH_CMD_READ           0x03
+#define sFLASH_CMD_RDSR           0x05
+#define sFLASH_CMD_SE             0xD8
+#define sFLASH_CMD_BE             0xC7
+#define sFLASH_WIP_FLAG           0x01
+#define sFLASH_DUMMY_BYTE         0xA5
+#define sFLASH_SPI_PAGESIZE       0x100
+#define sFLASH_M25P128_ID         0x202018
+#define sFLASH_M25P64_ID          0x202017
+
+#define sFLASH_CS_LOW()       HAL_GPIO(sFLASH_CS_GPIO_PORT, sFLASH_CS_PIN)
+#define sFLASH_CS_HIGH()      GPIO_SetBits(sFLASH_CS_GPIO_PORT, sFLASH_CS_PIN)
+void sFLASH_DeInit(void);
+void sFLASH_Init(void);
+void sFLASH_EraseSector(uint32_t SectorAddr);
+void sFLASH_EraseBulk(void);
+void sFLASH_WritePage(uint8_t* pBuffer, uint32_t WriteAddr, uint32_t NumByteToWrite);
+void sFLASH_WriteBuffer(uint8_t* pBuffer, uint32_t WriteAddr, uint32_t NumByteToWrite);
+void sFLASH_ReadBuffer(uint8_t* pBuffer, uint32_t ReadAddr, uint32_t NumByteToRead);
+
+uint32_t sFLASH_ReadID(void);
+void sFLASH_StartReadSequence(uint32_t ReadAddr);
+uint8_t sFLASH_ReadByte(void);
+uint8_t sFLASH_SendByte(uint8_t byte);
+uint16_t sFLASH_SendHalfWord(uint16_t HalfWord);
+void sFLASH_WriteEnable(void);
+void sFLASH_WaitForWriteEnd(void);
+
+/* Reset Operations */
+#define RESET_ENABLE_CMD                     0x66
+#define RESET_MEMORY_CMD                     0x99
+
+/* Identification Operations */
+#define READ_ID_CMD                          0x9E
+#define READ_ID_CMD2                         0x9F
+#define MULTIPLE_IO_READ_ID_CMD              0xAF
+#define READ_SERIAL_FLASH_DISCO_PARAM_CMD    0x5A
+
+/* Read Operations */
+#define READ_CMD                             0x03
+#define READ_4_BYTE_ADDR_CMD                 0x13
+
+#define FAST_READ_CMD                        0x0B
+#define FAST_READ_DTR_CMD                    0x0D
+#define FAST_READ_4_BYTE_ADDR_CMD            0x0C
+
+#define DUAL_OUT_FAST_READ_CMD               0x3B
+#define DUAL_OUT_FAST_READ_DTR_CMD           0x3D
+#define DUAL_OUT_FAST_READ_4_BYTE_ADDR_CMD   0x3C
+
+#define DUAL_INOUT_FAST_READ_CMD             0xBB
+#define DUAL_INOUT_FAST_READ_DTR_CMD         0xBD
+#define DUAL_INOUT_FAST_READ_4_BYTE_ADDR_CMD 0xBC
+
+#define QUAD_OUT_FAST_READ_CMD               0x6B
+#define QUAD_OUT_FAST_READ_DTR_CMD           0x6D
+#define QUAD_OUT_FAST_READ_4_BYTE_ADDR_CMD   0x6C
+
+#define QUAD_INOUT_FAST_READ_CMD             0xEB
+#define QUAD_INOUT_FAST_READ_DTR_CMD         0xED
+#define QUAD_INOUT_FAST_READ_4_BYTE_ADDR_CMD 0xEC
+
+/* Write Operations */
+#define WRITE_ENABLE_CMD                     0x06
+#define WRITE_DISABLE_CMD                    0x04
+
+/* Register Operations */
+#define READ_STATUS_REG_CMD                  0x05
+#define WRITE_STATUS_REG_CMD                 0x01
+
+#define READ_LOCK_REG_CMD                    0xE8
+#define WRITE_LOCK_REG_CMD                   0xE5
+
+#define READ_FLAG_STATUS_REG_CMD             0x70
+#define CLEAR_FLAG_STATUS_REG_CMD            0x50
+
+#define READ_NONVOL_CFG_REG_CMD              0xB5
+#define WRITE_NONVOL_CFG_REG_CMD             0xB1
+
+#define READ_VOL_CFG_REG_CMD                 0x85
+#define WRITE_VOL_CFG_REG_CMD                0x81
+
+#define READ_ENHANCED_VOL_CFG_REG_CMD        0x65
+#define WRITE_ENHANCED_VOL_CFG_REG_CMD       0x61
+
+#define READ_EXT_ADDR_REG_CMD                0xC8
+#define WRITE_EXT_ADDR_REG_CMD               0xC5
+
+/* Program Operations */
+#define PAGE_PROG_CMD                        0x02
+#define PAGE_PROG_4_BYTE_ADDR_CMD            0x12
+
+#define DUAL_IN_FAST_PROG_CMD                0xA2
+#define EXT_DUAL_IN_FAST_PROG_CMD            0xD2
+
+#define QUAD_IN_FAST_PROG_CMD                0x32
+#define EXT_QUAD_IN_FAST_PROG_CMD            0x12 /*0x38*/
+#define QUAD_IN_FAST_PROG_4_BYTE_ADDR_CMD    0x34
+
+/* Erase Operations */
+#define SUBSECTOR_ERASE_CMD                  0x20
+#define SUBSECTOR_ERASE_4_BYTE_ADDR_CMD      0x21
+
+#define SECTOR_ERASE_CMD                     0xD8
+#define SECTOR_ERASE_4_BYTE_ADDR_CMD         0xDC
+
+#define BULK_ERASE_CMD                       0xC7
+
+#define PROG_ERASE_RESUME_CMD                0x7A
+#define PROG_ERASE_SUSPEND_CMD               0x75
+
+/* One-Time Programmable Operations */
+#define READ_OTP_ARRAY_CMD                   0x4B
+#define PROG_OTP_ARRAY_CMD                   0x42
+
+/* 4-byte Address Mode Operations */
+#define ENTER_4_BYTE_ADDR_MODE_CMD           0xB7
+#define EXIT_4_BYTE_ADDR_MODE_CMD            0xE9
+
+/* Quad Operations */
+#define ENTER_QUAD_CMD                       0x35
+#define EXIT_QUAD_CMD                        0xF5
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ Core/Src/spidriver.c
@@ -0,0 +1,181 @@
+#include "spidriver.h"
+#include "stm32h7xx_hal_qspi.h"
+#include "quadspi.h"
+
+extern QSPI_HandleTypeDef hqspi;
+
+static void QSPI_WriteEnable(QSPI_HandleTypeDef *hqspi) {
+	QSPI_CommandTypeDef sCommand;
+	QSPI_AutoPollingTypeDef sConfig;
+
+	sCommand.InstructionMode = QSPI_INSTRUCTION_1_LINE;
+	sCommand.Instruction = WRITE_ENABLE_CMD;
+	sCommand.AddressMode = QSPI_ADDRESS_NONE;
+	sCommand.AlternateByteMode = QSPI_ALTERNATE_BYTES_NONE;
+	sCommand.DataMode = QSPI_DATA_NONE;
+	sCommand.DummyCycles = 0;
+	sCommand.DdrMode = QSPI_DDR_MODE_DISABLE;
+	sCommand.DdrHoldHalfCycle = QSPI_DDR_HHC_ANALOG_DELAY;
+	sCommand.SIOOMode = QSPI_SIOO_INST_EVERY_CMD;
+
+	if (HAL_QSPI_Command(hqspi, &sCommand, HAL_QPSI_TIMEOUT_DEFAULT_VALUE)
+			!= HAL_OK) {
+		Error_Handler();
+	}
+
+	sConfig.Match = 0x02;
+	sConfig.Mask = 0x02;
+	sConfig.MatchMode = QSPI_MATCH_MODE_AND;
+	sConfig.StatusBytesSize = 1;
+	sConfig.Interval = 0x10;
+	sConfig.AutomaticStop = QSPI_AUTOMATIC_STOP_ENABLE;
+
+	sCommand.Instruction = READ_STATUS_REG_CMD;
+	sCommand.DataMode = QSPI_DATA_1_LINE;
+
+	if (HAL_QSPI_AutoPolling(hqspi, &sCommand, &sConfig,
+	HAL_QPSI_TIMEOUT_DEFAULT_VALUE) != HAL_OK) {
+		Error_Handler();
+	}
+}
+
+static void QSPI_AutoPollingMemReady(QSPI_HandleTypeDef *hqspi) {
+	QSPI_CommandTypeDef sCommand;
+	QSPI_AutoPollingTypeDef sConfig;
+
+	sCommand.InstructionMode = QSPI_INSTRUCTION_1_LINE;
+	sCommand.Instruction = READ_STATUS_REG_CMD;
+	sCommand.AddressMode = QSPI_ADDRESS_NONE;
+	sCommand.AlternateByteMode = QSPI_ALTERNATE_BYTES_NONE;
+	sCommand.DataMode = QSPI_DATA_1_LINE;
+	sCommand.DummyCycles = 0;
+	sCommand.DdrMode = QSPI_DDR_MODE_DISABLE;
+	sCommand.DdrHoldHalfCycle = QSPI_DDR_HHC_ANALOG_DELAY;
+	sCommand.SIOOMode = QSPI_SIOO_INST_EVERY_CMD;
+
+	sConfig.Match = 0x00;
+	sConfig.Mask = 0x01;
+	sConfig.MatchMode = QSPI_MATCH_MODE_AND;
+	sConfig.StatusBytesSize = 1;
+	sConfig.Interval = 0x10;
+	sConfig.AutomaticStop = QSPI_AUTOMATIC_STOP_ENABLE;
+
+	if (HAL_QSPI_AutoPolling_IT(hqspi, &sCommand, &sConfig) != HAL_OK) {
+		Error_Handler();
+	}
+}
+
+static void initCommonCmd(QSPI_CommandTypeDef *cmd) {
+	cmd->InstructionMode = QSPI_INSTRUCTION_1_LINE;
+	cmd->AddressSize = QSPI_ADDRESS_24_BITS;
+	cmd->AlternateByteMode = QSPI_ALTERNATE_BYTES_NONE;
+	cmd->DdrMode = QSPI_DDR_MODE_DISABLE;
+	cmd->DdrHoldHalfCycle = QSPI_DDR_HHC_ANALOG_DELAY;
+	cmd->SIOOMode = QSPI_SIOO_INST_EVERY_CMD;
+}
+void sFLASH_DeInit(void) {
+	sFLASH_LowLevel_DeInit();
+	QSPI_WriteEnable(&hqspi);
+}
+
+void sFLASH_Init(void) {
+	QSPI_WriteEnable(&hqspi);
+	QSPI_AutoPollingMemReady(&hqspi);
+
+}
+
+void sFLASH_EraseSector(uint32_t SectorAddr) {
+	QSPI_CommandTypeDef sCommand;
+	initCommonCmd(&sCommand);
+	sCommand.Instruction = SECTOR_ERASE_CMD;
+	sCommand.AddressMode = QSPI_ADDRESS_4_LINES;
+	sCommand.Address = SectorAddr;
+	sCommand.DataMode = QSPI_DATA_NONE;
+	sCommand.DummyCycles = 0;
+	if (HAL_QSPI_Command_IT(&hqspi, &sCommand) != HAL_OK) {
+		Error_Handler();
+	}
+	QSPI_AutoPollingMemReady(&hqspi);
+}
+
+void sFLASH_EraseBulk(void) {
+	QSPI_CommandTypeDef sCommand;
+	initCommonCmd(&sCommand);
+	sCommand.Instruction = BULK_ERASE_CMD;
+	sCommand.AddressMode = QSPI_ADDRESS_NONE;
+	sCommand.DataMode = QSPI_DATA_NONE;
+	sCommand.DummyCycles = 0;
+	if (HAL_QSPI_Command_IT(&hqspi, &sCommand) != HAL_OK) {
+		Error_Handler();
+	}
+	QSPI_AutoPollingMemReady(&hqspi);
+}
+
+void sFLASH_WritePage(uint8_t *pBuffer, uint32_t WriteAddr,
+		uint32_t NumByteToWrite) {
+	QSPI_CommandTypeDef sCommand;
+	initCommonCmd(&sCommand);
+	sCommand.Instruction = PAGE_PROG_CMD;
+	sCommand.AddressMode = QSPI_ADDRESS_4_LINES;
+	sCommand.DataMode = QSPI_DATA_4_LINES;
+	sCommand.NbData = NumByteToWrite;
+	sCommand.DummyCycles = 0;
+	if (HAL_QSPI_Command_IT(&hqspi, &sCommand) != HAL_OK) {
+		Error_Handler();
+	}
+	HAL_QSPI_Transmit_IT(&hqspi, pBuffer);
+	QSPI_AutoPollingMemReady(&hqspi);
+}
+
+void sFLASH_WriteBuffer(uint8_t *pBuffer, uint32_t WriteAddr,
+		uint32_t NumByteToWrite) {
+	QSPI_CommandTypeDef sCommand;
+	initCommonCmd(&sCommand);
+	sCommand.Instruction = PAGE_PROG_CMD;
+	sCommand.AddressMode = QSPI_ADDRESS_4_LINES;
+	sCommand.Address = WriteAddr;
+	sCommand.DataMode = QSPI_DATA_4_LINES;
+	sCommand.NbData = NumByteToWrite;
+	sCommand.DummyCycles = 0;
+	if (HAL_QSPI_Command_IT(&hqspi, &sCommand) != HAL_OK) {
+		Error_Handler();
+	}
+	HAL_QSPI_Transmit_IT(&hqspi, pBuffer);
+	QSPI_AutoPollingMemReady(&hqspi);
+}
+
+void sFLASH_ReadBuffer(uint8_t *pBuffer, uint32_t ReadAddr,
+		uint32_t NumByteToRead) {
+	QSPI_CommandTypeDef sCommand;
+	initCommonCmd(&sCommand);
+	sCommand.Instruction = READ_CMD;
+	sCommand.AddressMode = QSPI_ADDRESS_4_LINES;
+	sCommand.Address = ReadAddr;
+	sCommand.DataMode = QSPI_DATA_4_LINES;
+	sCommand.NbData = NumByteToRead;
+	sCommand.DummyCycles = 0;
+	if (HAL_QSPI_Command_IT(&hqspi, &sCommand) != HAL_OK) {
+		Error_Handler();
+	}
+	HAL_QSPI_Receive_IT(&hqspi, pBuffer);
+	QSPI_AutoPollingMemReady(&hqspi);
+}
+
+uint32_t sFLASH_ReadID(void) {
+	QSPI_CommandTypeDef sCommand;
+	initCommonCmd(&sCommand);
+	sCommand.Instruction = READ_CMD;
+	sCommand.AddressMode = QSPI_ADDRESS_NONE;
+	sCommand.DataMode = QSPI_DATA_4_LINES;
+	sCommand.NbData = 3;
+	sCommand.DummyCycles = 0;
+	if (HAL_QSPI_Command_IT(&hqspi, &sCommand) != HAL_OK) {
+		Error_Handler();
+	}
+	uint8_t pData[4];
+	uint32_t res;
+	res = (pData[0] << 16) | (pData[1] << 8) | pData[2];
+
+	HAL_QSPI_Receive_IT(&hqspi, pData);
+}
+
